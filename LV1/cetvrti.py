9617299387e5fa8e30ed3be7f7f607f993aa3fcf
AnteSuvala <antejunior2003@gmail.com>
2026-02-27 16:36:45 +0100

    Refactor word counting logic

--- LV1/cetvrti.py
+++ LV1/cetvrti.py
@@ -9,14 +9,16 @@ with open("song.txt", "r", encoding="utf-8") as datoteka:
 
         for rijec in rijeci_u_redu:
             rijec = rijec.lower()
-
             rijec = rijec.strip(string.punctuation)
-
             rijeci[rijec] = rijeci.get(rijec, 0) + 1
 
-jednom = [r for r, broj in rijeci.items() if broj == 1]
-print(jednom)
-# print("Broj riječi koje se pojavljuju samo jednom:", len(jednom))
-# print("Te riječi su:")
+jednom = []
+
+for rijec in rijeci:
+    if rijeci[rijec] == 1:
+        jednom.append(rijec)
+
+print("Broj riječi koje se pojavljuju samo jednom:", len(jednom))
+print("Te riječi su:")
 for r in jednom:
     print(r)
